8bf8695a036910660bc927b608ac8d7a95a6d505
Tagizade Orxan <tagizade.orxan@gmail.com>
2020-04-26 22:36:22 +0400

    ok

--- src/App.js
+++ src/App.js
@@ -1,66 +1,49 @@
 import React from 'react';
 import './index';
-import Timer from './Tuesday/Timer';
-import Menu from './Tuesday/Menu';
-import UserLogin from './Tuesday/UserLogin';
+import Timer from './Timer';
+import Menu from './Menu';
+import UserLogin from './UserLogin';
 import Counter from './Counter';
 import Dice from './Dice';
 import Navigation from './Navigation'
 import Slider from './Slider';
 
-  
 
 class App extends React.Component {
-state = {active:"counter"} ;
- 
+  state = { active: "" };
 
+  finder = (e) => {
+    this.setState({ active: e.target.innerText });
+  }
   render() {
-     
-  
-    return(
+
+    let data = {
+      Counter: <Counter />,
+      Dice: <Dice />,
+      Navigation: <Navigation />,
+      Slider: <Slider />,
+      Timer: <Timer />,
+      Menu: <Menu />,
+      UserLogin: <UserLogin />
+    }
+
+    return (
       <>
-      <div className="exercises">
-      <div onClick={()=>this.setState({active:"counter"})}  className="exercise">Counter</div>
-      <div onClick={()=>this.setState({active:"dice"})}  className="exercise">Dice</div>
-      <div onClick={()=>this.setState({active:"navigation"})}  className="exercise">Navigation</div>
-      <div onClick={()=>this.setState({active:"slider"})}  className="exercise">Slider</div>
-      <div onClick={()=>this.setState({active:"timer"})}  className="exercise">Timer</div>
-      <div onClick={()=>this.setState({active:"menu"})}  className="exercise">Menu</div>
-      <div onClick={()=>this.setState({active:"userlogin"})}  className="exercise">User Login</div>
-      
-       </div>
-       
-       <div className={this.state.active === 'counter' ? "visible" : "unvisible"}>
-        <Counter/>
-       </div>
-      
+        <div onClick={this.finder} className="exercises">
+          <div className="exercise">Counter</div>
+          <div className="exercise">Dice</div>
+          <div className="exercise">Navigation</div>
+          <div className="exercise">Slider</div>
+          <div className="exercise">Timer</div>
+          <div className="exercise">Menu</div>
+          <div className="exercise">UserLogin</div>
+        </div>
 
-       <div className={this.state.active === 'dice' ? "visible" : "unvisible"}>
-        <Dice/>
-       </div>
+        <div >
+          {data[this.state.active]}
+        </div>
 
-       <div className={this.state.active === 'navigation' ? "visible" : "unvisible"}>
-        <Navigation/>
-       </div>
-
-       <div className={this.state.active === 'slider' ? "visible" : "unvisible"}>
-        <Slider/>
-       </div>
-
-       <div className={this.state.active === 'timer' ? "visible" : "unvisible"}>
-        <Timer/>
-       </div>
-
-       <div className={this.state.active === 'menu' ? "visible" : "unvisible"}>
-        <Menu/>
-       </div>
-
-       <div className={this.state.active === 'userlogin' ? "visible" : "unvisible"}>
-        <UserLogin/>
-       </div>
-
-
-     </>
+      </>
     )
   }
 }
